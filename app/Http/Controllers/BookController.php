e81e4f6c889c340e070ee4947d5c2e8da678598a
riof1210 <riofadli097@gmail.com>
2021-11-13 05:13:14 +0700

    Larapus Crud Buku

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,11 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        //mengambil data 'book' dan juga 'author'
+        //yang berelasi melalui method 'author'
+        //yang berasal dari model 'Book'
+        $books = Book::with('author')->get();
+        return view('admin.book.index', compact('books'));
     }
 
     /**
@@ -24,7 +29,9 @@ class BookController extends Controller
      */
     public function create()
     {
-        //
+        //mengambil data author
+        $author = Author::all();
+        return view('admin.book.create', compact('author'));
     }
 
     /**
@@ -35,7 +42,26 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|unique:books',
+            'amount' => 'required',
+            'author_id' => 'required',
+            'cover' => 'required|image|max:2048',
+        ]);
+
+        $book = new Book;
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        // upload image / foto
+        if ($request->hasFile('cover')) {
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/books/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+        return redirect()->route('books.index');
     }
 
     /**
@@ -44,9 +70,10 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function show(Book $book)
+    public function show($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        return view('admin.book.show', compact('book'));
     }
 
     /**
@@ -55,9 +82,11 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function edit(Book $book)
+    public function edit($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $author = Author::all();
+        return view('admin.book.edit', compact('book', 'author'));
     }
 
     /**
@@ -67,9 +96,28 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Book $book)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'amount' => 'required',
+            'author_id' => 'required',
+        ]);
+
+        $book = Book::findOrFail($id);
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        //upload image/foto
+        if ($request->hasFile('cover')){
+            $book->deleteImage();
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('images/books/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+        return redirect()->route('books.index');
     }
 
     /**
@@ -78,8 +126,11 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Book $book)
+    public function destroy($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $book->deleteImage();
+        $book->delete();
+        return redirect()->route('books.index');
     }
 }
